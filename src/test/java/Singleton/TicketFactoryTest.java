51dc1af773155a3bd54ad1e7953154ad65c41c9b
ssw331 <2405353202@qq.com>
2023-12-24 22:14:14 +0800

    修改单元测试

--- src/test/java/Singleton/TicketFactoryTest.java
+++ src/test/java/Singleton/TicketFactoryTest.java
@@ -29,7 +29,7 @@ class TicketFactoryTest {
     void createTicket() {
         ticket = ticketFactory.getAdultTicket(date, "成人");
         System.out.println();
-        System.out.println("以上为创建票务日志，接下来进行状态检测");
+        System.out.println("以上为创建票务日志，接下来进行创建状态检测");
         System.out.println();
         ticketFactory.createTicket(date, "成人");
     }
@@ -37,24 +37,34 @@ class TicketFactoryTest {
     @org.junit.jupiter.api.Test
     void existStateDetect() {
         ticket = ticketFactory.getAdultTicket(date, "成人");
+        System.out.println("以上为创建票务日志，接下来进行存在状态检测");
         ticketFactory.existStateDetect(ticket);
     }
 
     @org.junit.jupiter.api.Test
     void getAdultTicket() {
+        System.out.println("享元模式测试");
+        ticket = ticketFactory.getAdultTicket(date, "成人");  // 避免重复创建对象，但相比抽象工厂，复用率下降了
+        System.out.println("享元模式：尝试再次创建相同票种");
         ticket = ticketFactory.getAdultTicket(date, "成人");  // 避免重复创建对象，但相比抽象工厂，复用率下降了
         set();
     }
 
     @org.junit.jupiter.api.Test
     void getChildTicket() {
+        System.out.println("享元模式测试");
         ticket = ticketFactory.getChildTicket(date, "儿童");  // 避免重复创建对象，但相比抽象工厂，复用率下降了
+        System.out.println("享元模式：尝试再次创建相同票种");
+        ticket = ticketFactory.getAdultTicket(date, "儿童");  // 避免重复创建对象，但相比抽象工厂，复用率下降了
         set();
     }
 
     @org.junit.jupiter.api.Test
     void getElderTicket() {
+        System.out.println("享元模式测试");
         ticket = ticketFactory.getElderTicket(date, "老人");  // 避免重复创建对象，但相比抽象工厂，复用率下降了
+        System.out.println("享元模式：尝试再次创建相同票种");
+        ticket = ticketFactory.getAdultTicket(date, "老人");  // 避免重复创建对象，但相比抽象工厂，复用率下降了
         set();
     }
 
