ec069a028812f45e06fdf39ed364c7c458f890dd
ssw331 <2405353202@qq.com>
2023-12-27 23:17:13 +0800

    补充解释器

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,9 @@
 import Flyweight.Ticket;
 import Singleton.TicketFactory;
+import interpreter.Context;
+import interpreter.CustomerExpression;
+import interpreter.CustomerType;
+import interpreter.Expression;
 
 import java.util.*;
 
@@ -12,7 +16,13 @@ public class Demo {
         Ticket ticket;
         double price = 100.0;
         String bonus = "";
+        Boolean isVIP = false;
+
+        Expression customerExpression = new CustomerExpression();
+        Context context = new Context();
+
         Scanner scanner = new Scanner(System.in);
+
         create:
         while (true) {
             System.out.println("请输入入园日期和要创建的票种（成人、儿童、老人）：");
@@ -51,6 +61,39 @@ public class Demo {
             scanner.nextLine();
             ticketFactory.destroyTicket(date, type);
         }
+
+        System.out.println("请输入购买者身份信息以及是否为VIP：");
+        if (scanner.hasNextLine()) {
+            String ag = scanner.nextLine();
+            String[] ags = ag.split(" ");
+            type = ags[0];
+            if (Objects.equals(ags[1], "是")) {
+                isVIP = true;
+            }
+        }
+
+        switch (type) {
+            case "成人":
+                context.setCustomerInfo("ADULT", isVIP);
+                break;
+            case "儿童":
+                context.setCustomerInfo("CHILD", isVIP);
+                break;
+            case "老人":
+                context.setCustomerInfo("OLD", isVIP);
+                break;
+            case "退伍军人":
+                context.setCustomerInfo("VETERAN", isVIP);
+                break;
+            case "残疾人":
+                context.setCustomerInfo("DISABLED", isVIP);
+                break;
+            default:
+                break;
+        }
+
+        double ticketPrice = customerExpression.interpret(context);
+        System.out.println("门票价格：" + ticketPrice);
         scanner.close();
     }
 }
