56f453a11fd423768471696be528287f1df927e0
汤可娴 <1135530278@qq.com>
2023-12-31 12:07:13 +0800

    更新测试

--- src/main/java/amusementparkvisit/test/AmusementParkVisitTest.java
+++ src/main/java/amusementparkvisit/test/AmusementParkVisitTest.java
@@ -33,20 +33,36 @@ public class AmusementParkVisitTest {
 
     @Test
     public void testMediatorPattern() {
+        // 测试前的介绍
         System.out.println("*****************************【中介者模式测试】****************************");
+        System.out.println("# 中介者模式是用来减少多个对象或类之间的通信复杂性。");
+        System.out.println("# 这种模式提供了一个中介者类，该类通常处理不同类之间的通信，并支持易于维护的代码。");
+        System.out.println("**********************************************************************");
+        System.out.println();
 
+        // 测试开始
+        System.out.println("---------------------------中介者模式测试开始----------------------------");
         QueueMediator.showQueueMonitorBoard();
         rollerCoaster.setWaitTime(45);
         merryGoRound.setWaitTime(20);
         QueueMediator.showQueueMonitorBoard();
-
+        // 测试结束
+        System.out.println("---------------------------中介者模式测试结束----------------------------");
         System.out.println("---------------------------------------------------------------------\n");
     }
 
+
     @Test
     public void testSpecificationPattern() {
+        // 测试前的介绍
         System.out.println("*****************************【规约模式测试】****************************");
+        System.out.println("# 规约模式是一种特定的设计模式，它允许业务规则可以以链式的方式组合使用。");
+        System.out.println("# 该模式使用单一规则/规约的组合来表示复杂的业务逻辑。");
+        System.out.println("**********************************************************************");
+        System.out.println();
 
+        // 测试开始
+        System.out.println("---------------------------规约模式测试开始----------------------------");
         ISpecification<Visitor> ageSpecification = new VisitorAgeSpecification(12, 18);
         ISpecification<Staff> roleSpecification = new StaffRoleSpecification("Security");
 
@@ -60,19 +76,32 @@ public class AmusementParkVisitTest {
         System.out.println("年龄规约测试结果： " + (isYoungVisitorSatisfied ? "通过" : "未通过"));
         System.out.println("角色规约测试结果： " + (isSecurityStaffSatisfied ? "通过" : "未通过"));
 
+        // 测试结束
+        System.out.println("---------------------------规约模式测试结束----------------------------");
         System.out.println("---------------------------------------------------------------------\n");
     }
 
+
     @Test
     public void testStrategyPattern() {
+        // 测试前的介绍
         System.out.println("*****************************【策略模式测试】****************************");
+        System.out.println("# 策略模式是一种设计模式，允许在运行时选择算法的行为。");
+        System.out.println("# 它定义了算法族，分别封装起来，让它们之间可以互相替换，此模式让算法的变化独立于使用算法的客户。");
+        System.out.println("**********************************************************************");
+        System.out.println();
 
+        // 测试开始
+        System.out.println("---------------------------策略模式测试开始----------------------------");
         visit.experienceRide();
         visit.setVisitStrategy(new MerryGoRoundStrategy());
         visit.experienceRide();
         visit.setVisitStrategy(new WaterSlideStrategy());
         visit.experienceRide();
 
+        // 测试结束
+        System.out.println("---------------------------策略模式测试结束----------------------------");
         System.out.println("---------------------------------------------------------------------\n");
     }
+
 }
