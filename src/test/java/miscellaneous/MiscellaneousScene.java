46d86f099c9d45fb40b34eb44d911912294cf3fa
ssw331 <2405353202@qq.com>
2023-12-31 12:01:37 +0800

    场景统合——1

---
+++ src/test/java/miscellaneous/MiscellaneousScene.java
@@ -0,0 +1,13 @@
+package miscellaneous;
+
+public class MiscellaneousScene {
+    public static void main(String[] args) {
+        System.out.println("其他设计模式的统一展示开始：");
+        System.out.println("------------------------------------------------------------------");
+        DataAccessObjectTest.dataAccessObjectTest();
+        System.out.println();
+        FrontControllerTest.frontControllerTest();
+        System.out.println();
+        TransferObjectTest.transferObjectTest();
+    }
+}
